4a9c1bf57b139abfc2750e827169d59c0adb900e
Jakub Klewicki <kuba.klewicki@gmail.com>
2023-09-23 01:42:03 +0200

    Colapse navbar when site change in mobile

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  
+  constructor(private renderer: Renderer2) { }
 
+  collapseNavbar() {
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+    if (navbarCollapse && navbarCollapse.classList.contains('show')) {
+      this.renderer.removeClass(navbarCollapse, 'show'); // Remove the 'show' class
+
+    }
+  }
 }
